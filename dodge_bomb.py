4afbf8fcff0c1069b7647c76567532eca1f9b92b
c0a2201328 <c0a2201328@edu.teu.ac.jp>
2023-11-28 16:54:57 +0900

    演習3　画像切り替え

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,7 +2,6 @@ import sys
 import pygame as pg
 import random
 
-
 WIDTH, HEIGHT = 1600, 900
 delta = {
      pg.K_UP: (0, -5),
@@ -23,12 +22,32 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         tate = False
     return yoko, tate
 
+def kk_k():
+    kk_img0 = pg.transform.rotozoom(pg.image.load("ex02/fig/3.png"), 0, 2.0)
+    kk_img1 = pg.transform.flip(kk_img0, True, False)
+    return{
+        (0, 0):kk_img0,
+        (-5, 0):kk_img0,
+        (-5, -5):pg.transform.rotozoom(kk_img0, -45, 1.0),
+        (-5, +5):pg.transform.rotozoom(kk_img0, 45, 1.0),
+        (0, -5):pg.transform.rotozoom(kk_img1, 90, 1.0),
+        (+5, -5):pg.transform.rotozoom(kk_img1, 45, 1.0),
+        (+5, 0):pg.transform.rotozoom(kk_img1, 0, 1.0),
+        (+5, +5):pg.transform.rotozoom(kk_img1, -45, 1.0),
+        (0, +5):pg.transform.rotozoom(kk_img1, -90, 1.0)
+        }
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_img1 = pg.transform.rotozoom(kk_img, 180, 2.0)
+    kk_img_ch = pg.image.load("ex02/fig/6.png")
+    kk_imgs = kk_k()
+    kk_img = kk_imgs[(0,0)]
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
     bb_img = pg.Surface((20, 20))
@@ -40,11 +59,17 @@ def main():
     clock = pg.time.Clock()
     vx, vy = +5, +5
     tmr = 0
+    
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):
+            kk_img = kk_img_ch
+            #screen.fill((255,0,0))
+            screen.blit(kk_img, kk_rct)
+            pg.display.update()
             print("Game Over")
             return    
         key_lst = pg.key.get_pressed()
@@ -53,8 +78,10 @@ def main():
             if key_lst[k]:   #キーがおされたら
                 sum_mv[0] += tpl[0]
                 sum_mv[1] += tpl[1]
-
-            
+        accs = [a for a in range(1,11)]        
+        avx, avy = vx*accs[min(tmr//500, 9)], vy*accs[min(tmr//500, 9)]   
+        vx, vy = avx, avy     
+                   
 
         screen.blit(bg_img, [0, 0])
         kk_rct.move_ip(sum_mv[0], sum_mv[1])
